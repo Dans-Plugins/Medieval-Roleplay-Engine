de6d9a80fa38376db0f75108062138f73a55dcea
dmccoystephenson <dmccoystephenson@gmail.com>
2020-06-04 18:59:35 -0600

    added Character Card class with name

---
+++ src/plugin/CharacterCard.java
@@ -0,0 +1,13 @@
+package plugin;
+
+public class CharacterCard {
+    private String name;
+
+    void setName(String newName) {
+        name = newName;
+    }
+
+    String getName() {
+        return name;
+    }
+}
