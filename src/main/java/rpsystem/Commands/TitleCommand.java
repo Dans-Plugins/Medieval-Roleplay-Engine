c26851eb695042e14f4ecb461efd4325b7149d2c
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-16 21:54:49 -0600

    added TitleCommand.java

---
+++ src/main/java/rpsystem/Commands/TitleCommand.java
@@ -0,0 +1,13 @@
+package rpsystem.Commands;
+
+public class TitleCommand {
+    Main main = null;
+
+    public TitleCommand(Main plugin) {
+        main = plugin;
+    }
+
+    public void titleBook(CommandSender sender, String[] args) {
+
+    }
+}
