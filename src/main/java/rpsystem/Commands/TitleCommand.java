39a163a854aa8ccdbd838e7d042ee4aff64c3fec
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-16 22:28:58 -0600

    incremented version

--- src/main/java/rpsystem/Commands/TitleCommand.java
+++ src/main/java/rpsystem/Commands/TitleCommand.java
@@ -1,5 +1,13 @@
 package rpsystem.Commands;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import rpsystem.Main;
+
 public class TitleCommand {
     Main main = null;
 
@@ -9,5 +17,54 @@ public class TitleCommand {
 
     public void titleBook(CommandSender sender, String[] args) {
 
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+
+            // check permission
+            if (player.hasPermission("rp.title") || player.hasPermission("rp.default")) {
+
+                // check if they're holding a book
+                if (player.getInventory().getItemInMainHand().getType() == Material.BOOK) {
+
+                    // args check
+                    if (args.length > 0) {
+
+                        String newTitle = createStringFromArgs(args);
+
+                        ItemStack book = player.getInventory().getItemInMainHand();
+
+                        ItemMeta meta = book.getItemMeta();
+
+                        meta.setDisplayName(newTitle);
+
+                        book.setItemMeta(meta);
+
+                        player.getInventory().setItemInMainHand(book);
+
+                        player.sendMessage(ChatColor.GREEN + "Title added to book!");
+                    }
+                    else {
+                        player.sendMessage(ChatColor.RED + "Usage: /title (new title)");
+                    }
+
+                }
+                else {
+                    player.sendMessage(ChatColor.RED + "You have to be holding a book to use this command!");
+                }
+
+            }
+            else {
+                player.sendMessage(ChatColor.RED + "Sorry! In order to use this command, you need the following permission: 'rp.title'");
+            }
+        }
+
+    }
+
+    public String createStringFromArgs(String[] args) {
+        String toReturn = args[0];
+        for (int i = 1; i < args.length; i++) {
+            toReturn = toReturn + " " + args[i];
+        }
+        return toReturn;
     }
 }
