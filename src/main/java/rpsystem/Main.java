adc93b207276727c824b3e8ea52253570331f4bb
dmccoystephenson <dmccoystephenson@gmail.com>
2020-11-27 10:54:59 -0700

    made version v1.5

--- src/main/java/rpsystem/Main.java
+++ src/main/java/rpsystem/Main.java
@@ -24,7 +24,7 @@ import java.util.UUID;
 public class Main extends JavaPlugin implements Listener {
 
     // version
-    public String version = "v1.5-beta";
+    public String version = "v1.5";
 
     // subsystems
     public StorageSubsystem storage = new StorageSubsystem(this);
