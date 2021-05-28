b17613c5dd0b965a1fcebaddae5f62566f39c704
dmccoystephenson <dmccoystephenson@gmail.com>
2021-05-28 15:37:50 -0600

    created skeleton code for treating messages containing the "*" string differently than other messages

--- src/main/java/dansplugins/rpsystem/eventhandlers/AsyncPlayerChatEventHandler.java
+++ src/main/java/dansplugins/rpsystem/eventhandlers/AsyncPlayerChatEventHandler.java
@@ -6,6 +6,7 @@ import dansplugins.rpsystem.Messenger;
 import dansplugins.rpsystem.data.EphemeralData;
 import dansplugins.rpsystem.data.PersistentData;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,7 +22,21 @@ public class AsyncPlayerChatEventHandler implements Listener {
         int localChatRadius = MedievalRoleplayEngine.getInstance().getConfig().getInt("localChatRadius");
         String localChatColor = MedievalRoleplayEngine.getInstance().getConfig().getString("localChatColor");
         if (EphemeralData.getInstance().getPlayersSpeakingInLocalChat().contains(event.getPlayer().getUniqueId())) {
-            Messenger.getInstance().sendRPMessageToPlayersWithinDistance(event.getPlayer(), ColorChecker.getInstance().getColorByName(localChatColor) + "" + String.format("%s: \"%s\"", PersistentData.getInstance().getCard(event.getPlayer().getUniqueId()).getName(), event.getMessage()), localChatRadius);
+            // get color and character name
+            ChatColor color = ColorChecker.getInstance().getColorByName(localChatColor);
+            String characterName = PersistentData.getInstance().getCard(event.getPlayer().getUniqueId()).getName();
+
+            // prepare message to send
+            String messageToSend;
+            if (!event.getMessage().contains("*")) {
+                messageToSend = color + "" + String.format("%s: \"%s\"", characterName, event.getMessage());
+            }
+            else {
+                // TODO: implement emote-while-chatting feature
+                messageToSend = "";
+            }
+
+            Messenger.getInstance().sendRPMessageToPlayersWithinDistance(event.getPlayer(), messageToSend, localChatRadius);
             event.setCancelled(true);
             return;
         }
