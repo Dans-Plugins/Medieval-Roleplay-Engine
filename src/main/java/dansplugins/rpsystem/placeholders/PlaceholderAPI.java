b70b2389fe3c6286d0c410ff66d7ecffa6975852
Phil Garner <phil.b.garner@gmail.com>
2021-06-28 22:02:55 -0400

    Added PlaceholderAPI support and 6 initial placeholder tokens: card_name, card_age, card_race, card_subculture, card_gender, card_religion.

---
+++ src/main/java/dansplugins/rpsystem/placeholders/PlaceholderAPI.java
@@ -0,0 +1,64 @@
+package dansplugins.rpsystem.placeholders;
+
+import dansplugins.rpsystem.MedievalRoleplayEngine;
+import dansplugins.rpsystem.data.PersistentData;
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class PlaceholderAPI extends PlaceholderExpansion {
+
+    @Override
+    public @NotNull String getIdentifier() {
+        return MedievalRoleplayEngine.getInstance().getName();
+    }
+
+    @Override
+    public @NotNull String getAuthor() {
+        return String.join(", ", MedievalRoleplayEngine.getInstance().getDescription().getAuthors());
+    }
+
+    @Override
+    public @NotNull String getVersion() {
+        return MedievalRoleplayEngine.getInstance().getVersion();
+    }
+
+    @Override
+    public boolean persist() {
+        return true;
+    }
+
+    @Override
+    public boolean canRegister() {
+        return true;
+    }
+
+    @Override
+    public String onPlaceholderRequest(Player player, @NotNull String params) {
+
+        params = params.toLowerCase();
+
+        if (player == null) return null;
+
+        if (params.equalsIgnoreCase("card_name")) {
+            return PersistentData.getInstance().getCard(player.getUniqueId()).getName();
+        }
+        if (params.equalsIgnoreCase("card_age")) {
+            return Integer.toString(PersistentData.getInstance().getCard(player.getUniqueId()).getAge());
+        }
+        if (params.equalsIgnoreCase("card_race")) {
+            return PersistentData.getInstance().getCard(player.getUniqueId()).getRace();
+        }
+        if (params.equalsIgnoreCase("card_subculture")) {
+            return PersistentData.getInstance().getCard(player.getUniqueId()).getSubculture();
+        }
+        if (params.equalsIgnoreCase("card_gender")) {
+            return PersistentData.getInstance().getCard(player.getUniqueId()).getGender();
+        }
+        if (params.equalsIgnoreCase("card_religion")) {
+            return PersistentData.getInstance().getCard(player.getUniqueId()).getReligion();
+        }
+
+        return null;
+    }
+}
