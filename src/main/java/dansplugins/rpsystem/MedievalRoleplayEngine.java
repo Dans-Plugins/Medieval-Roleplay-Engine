c65efcd059c0d76f14047b95555211b73c4849ff
BuildTools <unconfigured@null.spigotmc.org>
2021-07-11 02:23:39 -0600

    changed version to v1.6.1-beta-2

--- src/main/java/dansplugins/rpsystem/MedievalRoleplayEngine.java
+++ src/main/java/dansplugins/rpsystem/MedievalRoleplayEngine.java
@@ -13,7 +13,7 @@ public class MedievalRoleplayEngine extends JavaPlugin {
     private static MedievalRoleplayEngine instance;
 
     // version
-    private String version = "v1.6.1-beta-1";
+    private String version = "v1.6.1-beta-2";
 
     public static MedievalRoleplayEngine getInstance() {
         return instance;
