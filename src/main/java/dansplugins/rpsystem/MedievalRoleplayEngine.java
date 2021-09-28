c0f40af4dfc7e0d9ac5b16d07b92ae73cba2b66f
dmccoystephenson <dmccoystephenson@gmail.com>
2021-09-28 16:18:02 -0600

    Changed version to v1.9-beta-2

--- src/main/java/dansplugins/rpsystem/MedievalRoleplayEngine.java
+++ src/main/java/dansplugins/rpsystem/MedievalRoleplayEngine.java
@@ -16,7 +16,7 @@ public class MedievalRoleplayEngine extends JavaPlugin {
     private static MedievalRoleplayEngine instance;
 
     // version
-    private String version = "v1.9-beta-1";
+    private String version = "v1.9-beta-2";
 
     public static MedievalRoleplayEngine getInstance() {
         return instance;
