9e310d546a46069f70c0e4da423f0715f88ae463
dmccoystephenson <dmccoystephenson@gmail.com>
2021-11-28 16:20:21 -0700

    Altered StorageManager.java

--- src/main/java/dansplugins/rpsystem/managers/StorageManager.java
+++ src/main/java/dansplugins/rpsystem/managers/StorageManager.java
@@ -1,14 +1,18 @@
 package dansplugins.rpsystem.managers;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 import dansplugins.rpsystem.MedievalRoleplayEngine;
 import dansplugins.rpsystem.data.PersistentData;
 import dansplugins.rpsystem.objects.RPCharacter;
 import dansplugins.rpsystem.objects.deprecated.CharacterCard;
 import dansplugins.rpsystem.utils.Logger;
-import preponderous.ponder.misc.JsonWriterReader;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 
 public class StorageManager {
@@ -16,7 +20,8 @@ public class StorageManager {
     private static StorageManager instance;
     private final static String FILE_PATH = "./plugins/MedievalRoleplayEngine/";
     private final static String CHARACTERS_FILE_NAME = "characters.json";
-    private JsonWriterReader jsonWriterReader = new JsonWriterReader();
+    private final Gson gson = (new GsonBuilder()).setPrettyPrinting().create();
+    private static Type LIST_MAP_TYPE = (new TypeToken<ArrayList<HashMap<String, String>>>() {}).getType();
 
     private StorageManager() {
 
@@ -51,12 +56,12 @@ public class StorageManager {
         for (RPCharacter character : PersistentData.getInstance().getCharacters()){
             characters.add(character.save());
         }
-        jsonWriterReader.writeOutFiles(characters, CHARACTERS_FILE_NAME);
+        writeOutFiles(characters, CHARACTERS_FILE_NAME);
     }
 
     private void loadCharacters() {
         PersistentData.getInstance().getCharacters().clear();
-        ArrayList<HashMap<String, String>> data = jsonWriterReader.loadDataFromFilename(FILE_PATH + CHARACTERS_FILE_NAME);
+        ArrayList<HashMap<String, String>> data = loadDataFromFilename(FILE_PATH + CHARACTERS_FILE_NAME);
         HashSet<RPCharacter> characters = new HashSet<>();
         for (Map<String, String> characterData : data){
             RPCharacter warning = new RPCharacter(characterData);
@@ -101,6 +106,31 @@ public class StorageManager {
         toReturn.setInfo("Religion", card.getReligion());
         toReturn.setInfo("Subculture", card.getSubculture());
         return toReturn;
+    }
+
+    private boolean writeOutFiles(List<Map<String, String>> saveData, String fileName) {
+        try {
+            File parentFolder = new File(FILE_PATH);
+            parentFolder.mkdir();
+            File file = new File(FILE_PATH, fileName);
+            file.createNewFile();
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8);
+            outputStreamWriter.write(this.gson.toJson(saveData));
+            outputStreamWriter.close();
+            return true;
+        } catch (IOException var6) {
+            return false;
+        }
+    }
+
+    private ArrayList<HashMap<String, String>> loadDataFromFilename(String filename) {
+        try {
+            Gson gson = (new GsonBuilder()).setPrettyPrinting().create();
+            JsonReader reader = new JsonReader(new InputStreamReader(new FileInputStream(filename), StandardCharsets.UTF_8));
+            return (ArrayList)gson.fromJson(reader, LIST_MAP_TYPE);
+        } catch (FileNotFoundException var4) {
+            return new ArrayList();
+        }
     }
 
 }
