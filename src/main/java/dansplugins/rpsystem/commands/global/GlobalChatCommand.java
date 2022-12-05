87121f4bf795dee057a8f1507a592d4da7c4f63b
dmccoystephenson <dmccoystephenson@gmail.com>
2022-12-04 20:50:51 -0700

    Removed new chat system and reverted back to legacy system.

--- src/main/java/dansplugins/rpsystem/commands/global/GlobalChatCommand.java
+++ src/main/java/dansplugins/rpsystem/commands/global/GlobalChatCommand.java
@@ -24,21 +24,6 @@ public class GlobalChatCommand {
             return false;
         }
 
-        if (!medievalRoleplayEngine.configService.getBoolean("legacyChat")) {
-
-            if (args.length != 0) {
-                if (args[0].equalsIgnoreCase("hide")) {
-                    addToPlayersWhoHaveHiddenGlobalChat(player);
-                    return true;
-                }
-                if (args[0].equalsIgnoreCase("show")) {
-                    removeFromPlayersWhoHaveHiddenGlobalChat(player);
-                    return true;
-                }
-            }
-
-        }
-
         // remove player from local chat
         removePlayerFromLocalChat(player);
 
@@ -52,26 +37,6 @@ public class GlobalChatCommand {
         }
         else {
             player.sendMessage(medievalRoleplayEngine.colorChecker.getNegativeAlertColor() + "You're already talking in global chat!");
-        }
-    }
-
-    private void addToPlayersWhoHaveHiddenGlobalChat(Player player) {
-        if (!medievalRoleplayEngine.ephemeralData.getPlayersWhoHaveHiddenGlobalChat().contains(player.getUniqueId())) {
-            medievalRoleplayEngine.ephemeralData.getPlayersWhoHaveHiddenGlobalChat().add(player.getUniqueId());
-            player.sendMessage(medievalRoleplayEngine.colorChecker.getPositiveAlertColor() + "Global chat is now hidden!");
-        }
-        else {
-            player.sendMessage(medievalRoleplayEngine.colorChecker.getNegativeAlertColor() + "Global chat is already hidden!");
-        }
-    }
-
-    private void removeFromPlayersWhoHaveHiddenGlobalChat(Player player) {
-        if (medievalRoleplayEngine.ephemeralData.getPlayersWhoHaveHiddenGlobalChat().contains(player.getUniqueId())) {
-            medievalRoleplayEngine.ephemeralData.getPlayersWhoHaveHiddenGlobalChat().remove(player.getUniqueId());
-            player.sendMessage(medievalRoleplayEngine.colorChecker.getPositiveAlertColor() + "Global chat is now visible!");
-        }
-        else {
-            player.sendMessage(medievalRoleplayEngine.colorChecker.getNegativeAlertColor() + "Global chat is already visible!");
         }
     }
 
